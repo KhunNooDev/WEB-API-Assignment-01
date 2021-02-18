5dffd8456751d73914d7137c2ef7ca14ccaeae02
woramet khaowphong <noo_okok@hotmail.com>
2021-02-18 18:15:33 +0700

    add index js

---
+++ index.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient
+const express = require('express')
+
+const app = express()
+const url = 'mongodb+srv://superadmin:1234567890123@cluster0.bmxvn.mongodb.net/sample_weatherdata?retryWrites=true&w=majority'
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+async function connect() {
+    //Create connection to mongodb
+    await client.connect()
+}
+connect()
+
+app.get('/weather', async (req, res) => {
+    try {
+        const callLetters = req.query.callLetters
+        const db = client.db('sample_weatherdata')
+        const collection = db.collection('data')
+        let query = {}
+        if (callLetters) {
+            query.callLetters = callLetters
+        }
+        const cursor = collection.find(query).limit(10)
+        let data = []
+        await cursor.forEach(doc => data.push(doc.name))
+        res.send(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+app.listen(3000, console.log('Start application at port 3000'))
